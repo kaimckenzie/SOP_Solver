613c4b9dad49975e563d7bb4ab5da54672273a89
Kai McKenzie <kaimckenzie@csus.edu>
2025-09-05 19:21:30 -0700

    add total enumerated node log output

--- lib/solver.cpp
+++ lib/solver.cpp
@@ -404,10 +404,11 @@ void solver::solve(string f_name, int thread_num)
             LKH_thread.join();
 
     // DIAGNOSTIC : Enumerated Nodes
-    //  unsigned long long enumerated_nodes_sum = 0;
-    //  for(int i = 0; i < enumerated_nodes.size(); i++){
-    //      enumerated_nodes_sum += enumerated_nodes[i];
-    //  }
+    unsigned long long enumerated_nodes_sum = 0;
+    for(int i = 0; i < enumerated_nodes.size(); i++){
+        enumerated_nodes_sum += enumerated_nodes[i];
+    }
+    std::cout << "Total enumerated nodes: " << enumerated_nodes_sum << std::endl;
 
     auto total_time = chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
     std::cout << "------------------------" << thread_total << " thread"
@@ -932,7 +933,7 @@ void solver::enumerate()
         }
 
         // DIAGNOSTIC: enum_nodes
-        // enumerated_nodes[thread_id] += ready_node_count;
+        enumerated_nodes[thread_id] += ready_node_count;
 
         // Sort the ready list and push into local pool
         if (!ready_list.empty())
