4f14e2adf6c68b5baa602b06118a2945327a8ef4
Kai McKenzie <kaimckenzie@csus.edu>
2025-09-10 23:44:04 -0700

    add preprocessor switch between spin lock and lock check for block count mutex

--- lib/block_count_mutex.cpp
+++ lib/block_count_mutex.cpp
@@ -3,10 +3,19 @@
 Block_Count_Mutex::Block_Count_Mutex() : blocked_count(0) {}
 
 void Block_Count_Mutex::lock() {
+#ifdef BLOCK_COUNT_SPIN_LOCK
+    // spin lock
     while (!mtx.try_lock()) {
         blocked_count++;
         std::this_thread::yield();
     }
+#else
+    // lock check
+    if (!mtx.try_lock()) {
+        blocked_count++;
+        mtx.lock();
+    }
+#endif
 }
 
 void Block_Count_Mutex::unlock() {
